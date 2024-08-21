68269f691b850c3e755aa42fae898ac31ce65791
crash1604 <cs.cucse@gmail.com>
2024-08-21 01:52:27 -0400

    working portfolio service

--- microservices/Portfolio/services/portfolioService.js
+++ microservices/Portfolio/services/portfolioService.js
@@ -1,18 +1,22 @@
 const Portfolio = require('../models/portfolioModel');
 
-exports.createPortfolio = async (portfolioData) => {
-    const portfolio = new Portfolio(portfolioData);
-    return await portfolio.save();
-};
+class PortfolioService {
+    createPortfolio = async (portfolioData) => {
+        const portfolio = new Portfolio(portfolioData);
+        return await portfolio.save();
+    };
 
-exports.getPortfoliosByUserId = async (userId) => {
-    return await Portfolio.find({ user_id: userId });
-};
+    async getPortfoliosByUserId(userId) {
+        return await Portfolio.find({ user_id: userId });
+    };
 
-exports.updatePortfolio = async (portfolioId, updatedData) => {
-    return await Portfolio.findOneAndUpdate({ portfolio_id: portfolioId }, updatedData, { new: true });
-};
+    async updatePortfolio(portfolioId, updatedData) {
+        return await Portfolio.findOneAndUpdate({ portfolio_id: portfolioId }, updatedData, { new: true });
+    };
 
-exports.deletePortfolio = async (portfolioId) => {
-    return await Portfolio.findOneAndDelete({ portfolio_id: portfolioId });
-};
+    async deletePortfolio(portfolioId) {
+        return await Portfolio.findOneAndDelete({ portfolio_id: portfolioId });
+    };
+}
+
+module.exports = new PortfolioService();
